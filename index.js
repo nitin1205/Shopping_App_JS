df0c931017bb8d3d9e58764e3506ccf2cc88310b
Nitin Singh <https://git.athmin.com/nitins/frontend-assignment.git>
2022-12-10 00:41:46 +0530

    setup project and added home route

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+	    <div>
+	      <form method="POST">
+	        <input name="email" placeholder="email" />
+		<input name="password" placeholder="password" />
+		<input name="confirmPassword" palceholder="confirm password" />
+		<button>Sign Up</button>
+	      </form>
+	    </div>`);
+});
+
+app.post('/', (req, res) => {
+    req.on('data', data => {
+        const parsedData = data.toString('utf8').split('&');
+	const formData = {};
+	    for (let pair of parsedData) {
+		const [key, value] = pair.split('=');
+		formData[key] = value;
+	    }
+        console.log(formData);
+    });
+    res.send('user added');
+});
+
+
+app.listen(3000, () => {
+    console.log('listening');
+});
