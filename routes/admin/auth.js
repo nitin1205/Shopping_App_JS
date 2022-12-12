da98317bb855e5b108f1f8fa240dc8c40bb292b2
Nitin Singh <vortechcorp@gmail.com>
2022-12-12 22:37:49 +0530

    added email and password validation using express-validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const userRepo = require('../../repositories/users.js');
+const { check, validationResult } = require('express-validator');
+const usersRepo = require('../../repositories/users.js');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
 
@@ -9,24 +10,48 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, confirmPassword } = req.body;
+router.post(
+  '/signup',
+  [
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must be a valid email')
+      .custom(async email => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error('Email already in use');
+        }
+      }),
+    check('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 charaters'),
+    check('confirmPassword')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters')
+      .custom((confirmPassword, { req }) => {
+        if  (confirmPassword !== req.body.password) {
+          throw new Error('Passwords must be same.');
+        }
+      })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);	  
+    const { email, password, confirmPassword } = req.body;
 
-  const existingUser = await usersRepo.getOneBy({ email });
 
-  if (existingUser) {
-    return res.send('Email already exists.');
-  }
 
-  if (password !== confirmPassword) {
-    return res.send('Passwords must be same.');
-  }
 
-  const user = await usersRepo.create({ email, password });
 
-  req.session.userId = user.id;
+    const user = await usersRepo.create({ email, password });
 
-  res.send('user added');
+    req.session.userId = user.id;
+
+    res.send('user added');
 });
 
 router.get('/signout', (req, res) =>  {
